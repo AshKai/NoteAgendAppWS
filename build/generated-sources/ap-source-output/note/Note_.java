1a563843f41a0fd220ece3ccc73355dda04a03c4
AshKai <thejaimes111dev@gmail.com>
2018-10-21 11:36:57 +0000

    finish version

---
+++ build/generated-sources/ap-source-output/note/Note_.java
@@ -0,0 +1,15 @@
+package note;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T10:02:28")
+@StaticMetamodel(Note.class)
+public class Note_ { 
+
+    public static volatile SingularAttribute<Note, Integer> id;
+    public static volatile SingularAttribute<Note, String> content;
+    public static volatile SingularAttribute<Note, String> email;
+
+}
